f802d9b291de440f70ee0539a6cba4bbb7e634a3
DevitoDbug <Davidochiengy@gmail.com>
2023-08-04 23:24:47 +0300

    Fixed error output statement

--- goErrorHandling/main.go
+++ goErrorHandling/main.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	num, err := strconv.Atoi("234th")
 	if err != nil {
-		fmt.Printf("lkajsdlfkjaldsjflakjdslfkajsd", err)
+		fmt.Printf("Can't convert: %q", err)
 		num = 0
 	}
 	fmt.Print(num)
