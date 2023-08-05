0bf6f84c9c38f064ecb8c4205026078e5d1a8ad6
DevitoDbug <Davidochiengy@gmail.com>
2023-08-05 10:12:40 +0300

    Fixed some bugs

--- goErrorHandlingErrorInterface/main.go
+++ goErrorHandlingErrorInterface/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 type divideError struct {
 	dividend    float64
@@ -15,22 +18,22 @@ func divide(dividend, divisor int) (Ans float64, err error) {
 	if dividend == 0 {
 		return 0, divideError{dividend: float64(dividend), contextFunc: "divide"}
 	}
-	return float64(dividend / dividend), nil
+	return float64(divisor / dividend), nil
 }
 
 func main() {
-	repeat := 'y'
+	repeat := "y"
 	divisor := 0
 	dividend := 0
 
-	for repeat == 'y' {
-		fmt.Print("Enter divisor: ")
+	for repeat == "y" {
+		fmt.Print("\nEnter divisor: ")
 		_, err := fmt.Scan(&divisor)
 		if err != nil {
 			fmt.Print(err)
 		}
 
-		fmt.Printf("Enter dividend: ")
+		fmt.Printf("\nEnter dividend: ")
 		_, err = fmt.Scan(&dividend)
 		if err != nil {
 			fmt.Print(err)
@@ -44,11 +47,13 @@ func main() {
 
 		fmt.Print("\n########################################################")
 		fmt.Print("\nDo you want to continue? Enter (y/n)")
-		_, err = fmt.Scanf("%s", repeat)
+		_, err = fmt.Scan(&repeat)
 		if err != nil {
 			fmt.Printf("%s\n", err)
-			repeat = 'n'
+			repeat = "n"
 		}
+		repeat = strings.TrimSpace(repeat)
+		fmt.Scanln()
 	}
 
 }
